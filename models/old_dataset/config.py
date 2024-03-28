69a2c40766cf879a94b95bf2a5b37842b6f4a65f
Kesha <kesha@vm229086.cloud.nuxt.network>
2024-03-28 13:28:53 +0100

    models added

---
+++ models/old_dataset/config.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from mingpt.model import GPT
+from mingpt.trainer import Trainer
+
+# files
+data_dir = Path(__file__).parent.joinpath('data')
+raw_data = data_dir.joinpath('anekdots.txt')
+stoi_file = data_dir.joinpath('stoi.json')
+itos_file = data_dir.joinpath('itos.json')
+model_path = data_dir.joinpath('model.pt')
+# special tokens
+# separator token
+begin_tkn = '[anek]'
+# token maximum length
+max_tkn_len = 10
+# token anekdote size and model's block_size
+max_anek_size = 16
+
+def get_model_config():
+    model_config = GPT.get_default_config()
+    model_config.model_type = 'gpt-mini'
+    # amount of tokens
+    model_config.vocab_size = 4096
+    model_config.block_size = max_anek_size
+    return model_config
+
+def get_train_config():
+    train_config = Trainer.get_default_config()
+    train_config.learning_rate = 6e-4
+    train_config.max_iters = 100000
+    train_config.batch_size = 32
+    train_config.num_workers = 2
+    return train_config
